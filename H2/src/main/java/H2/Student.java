ce7a3e408fe1bed2414b4b912f834c3032d35c23
Mahdavi Riccardo <45188583+MadaBebop@users.noreply.github.com>
2023-03-19 12:49:14 +0100

    Add files via upload

---
+++ H2/src/main/java/H2/Student.java
@@ -0,0 +1,55 @@
+package H2;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @Column(name = "surname", nullable = false)
+    String surname;
+
+    @Column(name = "email", nullable = false, unique = true)
+    String email;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
